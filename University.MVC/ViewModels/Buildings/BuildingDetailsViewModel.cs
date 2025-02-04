10e8052eb0976209e07cea03e06e1e7a2f8b10a0
jan.schuff <jan.schuff@vinci-energies.net>
2025-02-04 17:33:54 +0100

    Anpassung der Building Controller, Viewmodels und Views

---
+++ University.MVC/ViewModels/Buildings/BuildingDetailsViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using Cinema.Models;
+
+namespace Cinema.MVC.ViewModels.Buildings;
+
+public class BuildingListViewModel
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    [Display(Name = "Address")]
+    public string Address { get; set; }
+
+
+    public static BuildingListViewModel FromBuilding(Building building)
+    {
+        var buildingListViewModel = new BuildingListViewModel
+        {
+            Id = building.Id,
+            Name = building.Name,
+            Address = building.Address,
+        };
+
+        return buildingListViewModel;
+    }
+}
