6dd59757caa835584f24ac21fe1be60ee2ae3666
Niclas Schmidt <niclas0357@gmail.com>
2025-02-05 16:07:55 +0100

    Handler aktualisiert

--- University.Application/Revenue/GetRevenueQueryHandler.cs
+++ University.Application/Revenue/GetRevenueQueryHandler.cs
@@ -12,56 +12,18 @@ namespace Cinema.Application.Revenues;
 public class GetRevenueQueryHandler : IRequestHandler<GetRevenueQuery, Revenue>
 {
     private readonly CinemaContext context;
-    private readonly IDistributedCache cache;
 
-    private TimeSpan threeMonths = new(0, 3, 0, 0, 0, 0);
-
-    public GetRevenueQueryHandler(CinemaContext context, IDistributedCache cache)
+    public GetRevenueQueryHandler(CinemaContext context)
     {
         this.context = context;
-        this.cache = cache;
     }
 
     public async Task<Revenue> Handle(GetRevenueQuery request, CancellationToken cancellationToken)
     {
-        var revenue = await GetRevenueFromCache(request);
-        if (revenue != null)
-        {
-            return revenue;
-        }
-
-        revenue = await GetRevenueFromDatabase(request, cancellationToken);
-        await SetRevenueToCache(revenue);
-
-        return revenue;
-    }
-
-    private async Task SetRevenueToCache(Revenue revenue)
-    {
-        var key = $"revenue-{revenue.Id}";
-
-        var revenueAsSerializedJson = JsonSerializer.Serialize(revenue);
-
-        await cache.SetStringAsync(key, revenueAsSerializedJson, options: new DistributedCacheEntryOptions { SlidingExpiration = threeMonths });
-    }
-
-    private async Task<Revenue> GetRevenueFromCache(GetRevenueQuery request)
-    {
-        var key = $"revenue-{request.Id}";
-
-        var revenueAsSerializedJson = await cache.GetStringAsync(key);
-        if (revenueAsSerializedJson == null)
-        {
-            return null;
-        }
-
-        var revenue = JsonSerializer.Deserialize<Revenue>(revenueAsSerializedJson);
-        return revenue;
-    }
-
-    private async Task<Revenue> GetRevenueFromDatabase(GetRevenueQuery request, CancellationToken cancellationToken)
-    {
-        var revenue = await context.Revenues.FirstOrDefaultAsync(r => r.Id == request.Id, cancellationToken);
+        var revenue = await context.Revenues
+           .Include(revenue => revenue.Screening)
+           .FirstOrDefaultAsync(revenue => revenue.Id == request.Id, cancellationToken);
+        
         return revenue;
     }
 }
