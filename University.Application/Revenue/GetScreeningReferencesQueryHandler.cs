be2488a6b33be3b3fbc13526af38d83031b2afca
Niclas Schmidt <niclas0357@gmail.com>
2025-02-05 15:56:29 +0100

    Screening Reference zu Revenues hinzugefügt

---
+++ University.Application/Revenue/GetScreeningReferencesQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+
+using Microsoft.EntityFrameworkCore;
+
+using Cinema.Persistence;
+
+namespace Cinema.Application.Revenues;
+
+public class GetScreeningReferencesQueryHandler : IRequestHandler<GetScreeningReferencesQuery, ScreeningReferences>
+{
+    private readonly CinemaContext context;
+
+    public GetScreeningReferencesQueryHandler(CinemaContext context)
+    {
+        this.context = context;
+    }
+
+    public async Task<ScreeningReferences> Handle(GetScreeningReferencesQuery request, CancellationToken cancellationToken)
+    {
+        var allMovies = await context.Movies.ToListAsync(cancellationToken);
+
+        return new ScreeningReferences
+        {
+            Movies = allMovies,
+        };
+    }
+}
