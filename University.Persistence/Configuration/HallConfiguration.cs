ceecfc2a05e1839d9ddaaee8c0da85318af37442
Jannik Joos <joosj.wwi23@student.dhbw-heidenheim.de>
2025-02-06 13:58:01 +0100

    Configurations

---
+++ University.Persistence/Configuration/HallConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using Cinema.Models;
+
+namespace Cinema.Persistence.Configuration;
+
+public class HallConfiguration : IEntityTypeConfiguration<Hall>
+{
+    public void Configure(EntityTypeBuilder<Hall> builder)
+    {
+        builder.HasKey(hall => hall.Id);
+
+        builder.Property(hall => hall.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(hall => hall.Seats)
+            .IsRequired();
+
+        builder.HasOne(hall => hall.Building)
+            .WithMany(building => building.Halls)
+            .HasForeignKey(hall => hall.BuildingId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
